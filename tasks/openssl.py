6f9e15369791a41a8a8526734faee24e52cc683d
Tom Rothamel <pytom@bishoujo.us>
2019-11-25 01:03:00 -0500

    Build openssl.

---
+++ tasks/openssl.py
@@ -0,0 +1,27 @@
+from renpybuild.model import task
+
+version = "1.1.1d"
+
+
+@task()
+def unpack_openssl(c):
+    c.clean()
+
+    c.var("version", version)
+    c.run("tar xzf {{source}}/openssl-{{version}}.tar.gz")
+
+
+@task()
+def build_openssl(c):
+    c.var("version", version)
+    c.chdir("openssl-{{version}}")
+
+    if c.platform == "mac":
+        c.environ("KERNEL_BITS", "64")
+
+    c.run("""
+    ./config no-shared no-asm --prefix="{{ install }}"
+    """)
+
+    c.run("""make""")
+    c.run("""make install""")
