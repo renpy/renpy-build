115c77f92d9aaaf328031baaf243b5575ea53d5e
Tom Rothamel <pytom@bishoujo.us>
2020-01-22 02:03:57 -0500

    Add git update and rename the nightly script.

---
+++ nightly/git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+update () {
+
+    if [ -d "$BASE/$2/.git" ]; then
+        pushd "$BASE/$2"
+        git pull
+        popd
+    else
+        git clone $1 "$BASE/$2" --reference "$REFS/$2"
+    fi
+}
+
+update https://github.com/renpy/renpy-build
+
+update https://github.com/renpy/renpy renpy
+
+update https://github.com/renpy/pygame_sdl2 pygame_sdl2
+
+
+
