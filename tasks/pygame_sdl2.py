87aef4a70127e05d9f78df4da747f6130e12c16c
Tom Rothamel <pytom@bishoujo.us>
2020-01-22 02:15:35 -0500

    Use cython to generate pygame_sdl2.

--- tasks/pygame_sdl2.py
+++ tasks/pygame_sdl2.py
@@ -6,6 +6,14 @@ def annotate(c):
     c.include("{{ install }}/include/{{ pythonver }}/pygame_sdl2")
 
 
+@task(kind="python-only", always=True)
+def gen_static(c):
+
+    c.chdir("{{ renpy }}/module")
+    c.env("PYGAME_SDL2_STATIC", "1")
+    c.run("{{ hostpython }} setup.py generate")
+
+
 @task(kind="python", always=True)
 def install(c):
     c.run("{{ hostpython }} {{ pygame_sdl2 }}/setup.py install --single-version-externally-managed --record files.txt --no-extensions")
