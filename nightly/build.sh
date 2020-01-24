1949146e5ec962c6785ae0e782bbe2ec1df37ed4
Tom Rothamel <pytom@bishoujo.us>
2020-01-23 21:09:05 -0500

    Script to build a nightly distribution.

--- nightly/build.sh
+++ nightly/build.sh
@@ -11,8 +11,83 @@ if [ $ROOT != $BASE ]; then
     done
 fi
 
+# Build the dependencies.
 pushd $BASE
-
 ./all.sh
+popd
+
+
+link () {
+    if [ ! -L $2 ]; then
+        ln -s $1 $2
+    fi
+}
+
+pushd $BASE/renpy
+
+# Update the README.
+cp /home/tom/ab/renpy-deps/scripts/README.nightly /home/tom/ab/WWW.nightly/README.txt
+
+# Activate the virtualenv for the prebuild.
+. /home/tom/.virtualenvs/nightlyrenpy/bin/activate
+
+# Run the after checkout script.
+# ./after_checkout.sh
+
+link $BASE/pygame_sdl2 pygame_sdl2
+link /home/tom/ab/WWW.nightly dl
+link /home/tom/ab/renpy/atom atom
+link /home/tom/ab/renpy/jedit jedit
+link /home/tom/ab/renpy/editra editra
+
+# Rapt is needed to run distribute.py.
+mkdir -p rapt
+link /home/tom/ab/android/buildlib rapt/buildlib
+
+
+# Figure out a reasonable version name.
+REV=$(git rev-parse --short HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+export RENPY_NIGHTLY="nightly-$(date +%Y-%m-%d)-$REV"
+
+# Generate source.
+
+export RENPY_CYTHON=cython
+export RENPY_DEPS_INSTALL=/usr::/usr/lib/x86_64-linux-gnu/
+export RENPY_SIMPLE_EXCEPTIONS=1
+
+./renpy.sh tutorial quit
+
+# Build the documentation.
+# pushd $BASE/renpy/sphinx
+# ./build.sh
+# popd
+
+# Build the distribution.
+./lib/linux-x86_64/python -O distribute.py "$RENPY_NIGHTLY" --pygame $BASE/pygame_sdl2 --nosign
+
+popd
+
+# Create a symlink to the current nightly.
+
+pushd $BASE/renpy/dl
+
+rm current
+ln -s "$RENPY_NIGHTLY" current
+
+rm renpy-nightly-sdk.zip
+rm renpy-nightly-sdk.tar.bz2
+ln -s current/renpy-*-sdk.zip renpy-nightly-sdk.zip
+ln -s current/renpy-*-sdk.tar.bz2 renpy-nightly-sdk.tar.bz2
+
+# Index the nightly.
+/home/tom/ab/renpy-deps/scripts/index_nightly.py /home/tom/magnetic/ab/WWW.nightly/
+
+# Upload everything to the server.
+# rsync -av /home/tom/magnetic/ab/WWW.nightly/ tom@abagail.onegeek.org:/home/tom/WWW.nightly --delete
+
+# Delete old nightlies.
+# find /home/tom/magnetic/ab/WWW.nightly/ -ctime +30.5 -delete || true
 
 popd
