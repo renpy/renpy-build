b25f96b48ccc5305212bda5f54c687d5567a7cd5
Mal Graty <mal.graty@googlemail.com>
2020-02-06 22:16:59 +0000

    Remove duplicate condition

--- tasks/ffmpeg.py
+++ tasks/ffmpeg.py
@@ -20,8 +20,6 @@ def build(c):
         c.var("arch", "x86_64")
     elif c.arch == "armv7l":
         c.var("arch", "armhf")
-    elif c.arch == "armv7l":
-        c.var("arch", "armhf")
     elif c.arch == "arm64_v8a":
         c.var("arch", "aarch64")
     elif c.arch == "armeabi_v7a":
