132a7e3eef9fc9eb7c986e23d9c53cb3b58a47df
Tom Rothamel <pytom@bishoujo.us>
2020-02-11 00:28:29 -0500

    Allow for building with modified android java.

--- test_android.sh
+++ test_android.sh
@@ -18,6 +18,9 @@ export PGS4A_NO_TERMS=1
 python android.py installsdk
 popd
 
+rm -Rf "$ROOT/renpy/rapt/prototype/renpyandroid/src/main/java" || true
+cp -aL rapt/prototype/renpyandroid/src/main/java $ROOT/renpy/rapt/prototype/renpyandroid/src/main/
+
 if [ "$1" != "" ]; then
     $ROOT/renpy/renpy.sh $ROOT/renpy/launcher android_build "$1" installDebug --launch
 fi
