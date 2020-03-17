1dd1937e122a5d2dd1865fb286e4261f8636a428
Tom Rothamel <pytom@bishoujo.us>
2020-03-16 23:43:48 -0400

    Run ios apps remotely on the simulator.

---
+++ run_ios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+pushd /Volumes/shared/ab/renpy-build/renios/prototype
+xcodebuild -project prototype.xcodeproj/ -scheme prototype -configuration Debug -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.3'
+xcrun simctl install booted ~/Library/Developer/Xcode/DerivedData/prototype-ftosscwgctqkhgaylgxdbpdvtkdk/Build/Products/Debug-iphonesimulator/prototype.app
+xcrun simctl launch --console booted org.renpy.prototype
--- test_ios.sh
+++ test_ios.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-./build.py --platform ios
+set -e
+
+./build.py --platform ios rebuild renios pyobjus
 ./renpy/run.sh ./renpy/launcher ios_populate ./renpy/the_question ./renios/prototype/
+ssh tom@mary12 -t /Volumes/shared/ab/renpy-build/run_ios.sh
