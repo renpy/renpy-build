9632f25c7eeb0deb5a80f6eca29e51e9d0f47fb5
Tom Rothamel <pytom@bishoujo.us>
2020-03-18 22:35:48 -0400

    Disable auto launching on iOS.

--- test_ios.sh
+++ test_ios.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-./build.py --platform ios rebuild renios pyobjus
+./build.py --platform ios
 ./renpy/run.sh ./renpy/launcher ios_populate ./renpy/the_question ./renios/prototype/
-ssh tom@mary12 -t /Volumes/shared/ab/renpy-build/run_ios.sh
