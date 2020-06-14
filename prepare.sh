a67621ed728177a2b3ca0536215512998b2614f3
Tom Rothamel <pytom@bishoujo.us>
2020-06-14 16:50:13 -0400

    Add a script to install the dependences.

---
+++ prepare.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+
+ROOT=$(cd $(dirname $0); pwd)
+REFS=$ROOT
+BASE="$ROOT"
+
+# Needed to build things.
+sudo apt install -y git buld-essential ccache python-dev python3-dev unzip
+
+# Needed to install python2 pip
+sudo apt install -y curl
+
+# Needed by renpy-build itself.
+sudo apt install -y python3-jinja2
+
+# Needed by sysroot.
+apt install -y debootstrap qemu-user-static
+
+# Needed by gcc.
+sudo apt install -y libgmp-dev libmpfr-dev libmpc-dev
+
+# Needed by hostpython.
+sudo apt install -y libssl-dev libbz2-dev lib
+
+
+
+# Install the standard set of packages needed to build Ren'Py.
+sudo apt install -y \
+    python-dev libavcodec-dev libavformat-dev \
+    libavresample-dev libswresample-dev libswscale-dev libfreetype6-dev libglew1.6-dev \
+    libfribidi-dev libsdl2-dev libsdl2-image-dev libsdl2-gfx-dev \
+    libsdl2-mixer-dev libsdl2-ttf-dev libjpeg-turbo8-dev
+
+
+mkdir -p "$BASE/tmp"
+
+GET_PIP="$ROOT/tmp/get-pip.py"
+
+if [ ! -e $GET_PIP ]; then
+    curl https://bootstrap.pypa.io/get-pip.py -o $GET_PIP
+    sudo python2 $GET_PIP
+fi
+
+pip2 install virtualenv
+
+# Set up the environment variables.
+
+VENV="$ROOT/tmp/virtualenv.py2"
+
+export RENPY_DEPS_INSTALL=/usr::/usr/lib/x86_64-linux-gnu/
+
+. $BASE/nightly/git.sh
+
+cp /mnt/ab/renpy-build/nightly/python.sh nightly
+
+. $BASE/nightly/python.sh
