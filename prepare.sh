4d0125b8a8f1bcf4eb363eced1870752bd304f12
Mal Graty <mal.graty@googlemail.com>
2020-06-15 23:09:13 +0100

    Use apt-get in scripts as it's safer
    The apt binary is not advised for use in scripts as its interface is
    designed for humans and is not considered stable. It's recommended to
    use apt-get (and friends) in scripts as efforts are made to ensure
    compatibility with prior versions.
    
    ref: man 8 apt

--- prepare.sh
+++ prepare.sh
@@ -8,32 +8,32 @@ REFS=$ROOT
 BASE="$ROOT"
 
 # Needed to build things.
-sudo apt install -y git build-essential ccache python-dev python3-dev unzip
+sudo apt-get install -y git build-essential ccache python-dev python3-dev unzip
 
 # Needed to install python2 pip
-sudo apt install -y curl
+sudo apt-get install -y curl
 
 # Needed by renpy-build itself.
-sudo apt install -y python3-jinja2
+sudo apt-get install -y python3-jinja2
 
 # Needed by sysroot.
-sudo apt install -y debootstrap qemu-user-static
+sudo apt-get install -y debootstrap qemu-user-static
 
 # Needed by gcc.
-sudo apt install -y libgmp-dev libmpfr-dev libmpc-dev
+sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev
 
 # Needed by hostpython.
-sudo apt install -y libssl-dev libbz2-dev
+sudo apt-get install -y libssl-dev libbz2-dev
 
 # Needed for windows.
-sudo apt install -y mingw-w64 autoconf
+sudo apt-get install -y mingw-w64 autoconf
 
 # Needed for mac
 sudo apt install -y cmake clang libxml2-dev llvm
 
 
 # Install the standard set of packages needed to build Ren'Py.
-sudo apt install -y \
+sudo apt-get install -y \
     python-dev libavcodec-dev libavformat-dev \
     libavresample-dev libswresample-dev libswscale-dev libfreetype6-dev libglew1.6-dev \
     libfribidi-dev libsdl2-dev libsdl2-image-dev libsdl2-gfx-dev \
