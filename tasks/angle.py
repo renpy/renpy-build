825b5c666540459d986ad7d7b18f99f298a90b4a
Tom Rothamel <pytom@bishoujo.us>
2020-07-17 19:28:06 -0400

    Include the d3d compiler with the ANGLE files.

--- tasks/angle.py
+++ tasks/angle.py
@@ -6,3 +6,4 @@ def install(c):
     c.run("install -d {{ dlpa }}")
     c.run("install {{ prebuilt }}/{{ c.arch}}/libGLESv2.dll {{ dlpa }}")
     c.run("install {{ prebuilt }}/{{ c.arch }}/libEGL.dll {{ dlpa }}")
+    c.run("install {{ prebuilt }}/{{ c.arch }}/d3dcompiler_47.dll {{ dlpa }}")
