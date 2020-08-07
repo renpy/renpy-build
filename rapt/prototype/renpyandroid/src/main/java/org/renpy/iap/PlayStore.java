8e6580a060f7f5ce73da8c17e8cbb61af6d48edf
Tom Rothamel <pytom@bishoujo.us>
2020-08-06 23:36:34 -0400

    Update the Play Store to use Android Billing Library v3.

--- rapt/prototype/renpyandroid/src/main/java/org/renpy/iap/PlayStore.java
+++ rapt/prototype/renpyandroid/src/main/java/org/renpy/iap/PlayStore.java
@@ -1,61 +1,91 @@
 package org.renpy.iap;
 
-import java.util.ArrayList;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.android.vending.billing.IInAppBillingService;
+import com.android.billingclient.api.*;
+
 
 public class PlayStore extends Store {
 
-    /** True if we're finished with what we're trying to do. */
+    /**
+     * True if we're finished with what we're trying to do.
+     */
     boolean finished = true;
 
-    /** The activity we're associated with. */
+    /**
+     * The activity we're associated with.
+     */
     Activity activity;
 
-    /** The billing service. */
-    IInAppBillingService service;
+    /* The billingClient object, if it exists. */
+    private BillingClient billingClient = null;
 
-    /** The connection to the billing service. */
-    ServiceConnection connection = new ServiceConnection() {
-       @Override
-       public void onServiceDisconnected(ComponentName name) {
-           service = null;
-       }
-
-       @Override
-       public void onServiceConnected(ComponentName name, IBinder binder) {
-           service = IInAppBillingService.Stub.asInterface(binder);
-       }
-    };
+    /* A map from sku to SkuDetails object. */
+    private HashMap<String, SkuDetails> skuDetailsMap = new HashMap<>();
 
     public PlayStore(Activity activity) {
         this.activity = activity;
 
-        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.android.vending");
-        activity.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
+        final BillingClient bc = BillingClient.newBuilder(activity)
+                .setListener(purchasesUpdatedListener)
+                .enablePendingPurchases()
+                .build();
+
+        bc.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    billingClient = bc;
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                billingClient = null;
+            }
+        });
     }
+
+    private PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
+        @Override
+        public void onPurchasesUpdated(BillingResult billingResult, List<Purchase> purchases) {
+            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                if (purchases != null) {
+                    for (Purchase p : purchases) {
+                        handlePurchase(p);
+                    }
+                }
+            }
+        }
+    };
+
+    AcknowledgePurchaseResponseListener acknowledgePurchaseResponseListener = new AcknowledgePurchaseResponseListener() {
+        @Override
+        public void onAcknowledgePurchaseResponse(@NonNull BillingResult billingResult) {
+        }
+    };
+
+    void handlePurchase(Purchase purchase) {
+        if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED) {
+            if (!purchase.isAcknowledged()) {
+                AcknowledgePurchaseParams acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder().setPurchaseToken(purchase.getPurchaseToken()).build();
+                billingClient.acknowledgePurchase(acknowledgePurchaseParams, acknowledgePurchaseResponseListener);
+            }
+
+            purchased.add(purchase.getSku());
+        }
+    }
+
 
     @Override
     public void destroy() {
-        if (service != null) {
-            this.activity.unbindService(connection);
-        }
-    };
+    }
 
     @Override
     public boolean getFinished() {
@@ -67,72 +97,52 @@ public class PlayStore extends Store {
         return "play";
     }
 
-    public boolean isBillingSupported() {
-        if (service == null) {
-            return false;
-        }
 
+    @Override
+    public void updatePrices() {
         try {
-            int response = service.isBillingSupported(3, activity.getPackageName(), "inapp");
-            return response == 0; // RESULT_OK
-        } catch (RemoteException e) {
-            return false;
+
+            finished = false;
+
+            SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+            params.setSkusList(skus).setType(BillingClient.SkuType.INAPP);
+
+            billingClient.querySkuDetailsAsync(params.build(),
+                    new SkuDetailsResponseListener() {
+                        @Override
+                        public void onSkuDetailsResponse(@NonNull BillingResult billingResult, List<SkuDetails> skuDetailsList) {
+                            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                                prices.clear();
+
+                                for (SkuDetails sku : skuDetailsList) {
+                                    prices.put(sku.getSku(), sku.getPrice());
+                                    skuDetailsMap.put(sku.getSku(), sku);
+                                }
+
+                                finished = true;
+                            }
+                        }
+                    });
+
+        } catch (Exception e) {
+            finished = true;
+            Log.e("iap", "beginPurchase failed.", e);
         }
     }
-
-    public void updatePrices() {
-        finished = false;
-
-        new Thread() {
-            public void run() {
-                try {
-                    Bundle querySkus = new Bundle();
-                    querySkus.putStringArrayList("ITEM_ID_LIST", skus);
-
-                    Bundle skuDetails = service.getSkuDetails(3, activity.getPackageName(), "inapp", querySkus);
-                    int response = skuDetails.getInt("RESPONSE_CODE");
-
-                    if (response == 0) {
-                        ArrayList<String> responseList = skuDetails.getStringArrayList("DETAILS_LIST");
-
-                        prices.clear();
-
-                        for (String thisResponse : responseList) {
-                            JSONObject object = new JSONObject(thisResponse);
-                            String sku = object.getString("productId");
-                            String price = object.getString("price");
-
-                            Log.i("iap", "sku " + sku + " price " + price);
-                            prices.put(sku, price);
-                        }
-                    }
-
-                } catch (Exception e) {
-                    Log.e("iap", "getSkuDetails failed.", e);
-                }
-
-                finished = true;
-            }
-        }.start();
-
-    };
 
     @Override
     public void restorePurchases() {
         try {
-            Bundle ownedItems = service.getPurchases(3, activity.getPackageName(), "inapp", null);
 
-            int response = ownedItems.getInt("RESPONSE_CODE");
-
-            if (response == 0) {
-                ArrayList<String> ownedSkus = ownedItems.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
-
-                purchased.clear();
-
-                for (String s : ownedSkus) {
-                    purchased.add(s);
+            Purchase.PurchasesResult pr = billingClient.queryPurchases(BillingClient.SkuType.INAPP);
+            if (pr.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                if (pr.getPurchasesList() != null) {
+                    for (Purchase p : pr.getPurchasesList()) {
+                        handlePurchase(p);
+                    }
                 }
             }
+
         } catch (Exception e) {
             Log.e("iap", "restorePurchases failed.", e);
         }
@@ -144,56 +154,21 @@ public class PlayStore extends Store {
     public void beginPurchase(String sku) {
         try {
             finished = false;
-            Bundle buyIntentBundle = service.getBuyIntent(3, activity.getPackageName(), sku, "inapp", "via renpy.iap");
-            PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
 
-            activity.startIntentSenderForResult(
-                    pendingIntent.getIntentSender(),
-                    1001,
-                    new Intent(),
-                    Integer.valueOf(0),
-                    Integer.valueOf(0),
-                    Integer.valueOf(0));
+            // Retrieve a value for "skuDetails" by calling querySkuDetailsAsync().
+            BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                    .setSkuDetails(Objects.requireNonNull(skuDetailsMap.get(sku)))
+                    .build();
 
+            int responseCode = billingClient.launchBillingFlow(activity, billingFlowParams).getResponseCode();
+
+            if (responseCode != BillingClient.BillingResponseCode.OK) {
+                finished = true;
+            }
 
         } catch (Exception e) {
             finished = true;
             Log.e("iap", "beginPurchase failed.", e);
         }
     }
-
-    @Override
-    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1001) {
-            try {
-
-                int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
-                String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
-                // String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
-
-                Log.i("iap", "resultCode = " + resultCode + ", responseCode = " + responseCode);
-
-                if (resultCode == Activity.RESULT_OK && (responseCode == 0 || responseCode == 7)) {
-                    try {
-                        JSONObject jo = new JSONObject(purchaseData);
-                        String sku = jo.getString("productId");
-                        purchased.add(sku);
-                    }  catch (JSONException e) {
-                        Log.e("iap", "Failed to parse purchase data.", e);
-                    }
-                }
-
-            } finally {
-                finished = true;
-            }
-
-            return true;
-        }
-
-
-        return false;
-    }
-
-
-
 }
