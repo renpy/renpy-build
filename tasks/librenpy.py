08244f7c7f5a796fb9f3eec097c69d88cce3a738
Tom Rothamel <pytom@bishoujo.us>
2020-09-20 01:50:15 -0400

    Do not build pygame_sdl2 or renpy for the web platform.
    renpyweb should take care of that now.

--- tasks/librenpy.py
+++ tasks/librenpy.py
@@ -6,7 +6,7 @@ def clean(c):
     c.clean()
 
 
-@task(kind="host-python", platforms="all", always=True)
+@task(kind="host-python", always=True)
 def gen_static(c):
 
     c.chdir("{{ renpy }}/module")
