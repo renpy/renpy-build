04040033ac8b18b25e33fd9b5a11a1242e0641c1
Tom Rothamel <pytom@bishoujo.us>
2021-01-21 07:51:11 -0500

    Delete the gen/ directories when doing a nightly build.

--- nightly/git.sh
+++ nightly/git.sh
@@ -19,3 +19,14 @@ update https://github.com/renpy/renpy-build
 update https://github.com/renpy/renpy renpy
 update https://github.com/renpy/pygame_sdl2 pygame_sdl2
 update https://github.com/renpy/renpyweb renpyweb
+
+rm -Rf "$BASE/renpy/module/gen"
+rm -Rf "$BASE/renpy/module/gen3"
+rm -Rf "$BASE/renpy/module/gen-static"
+rm -Rf "$BASE/renpy/module/gen30-static"
+
+rm -Rf "$BASE/pygame_sdl2/gen"
+rm -Rf "$BASE/pygame_sdl2/gen3"
+rm -Rf "$BASE/pygame_sdl2/gen-static"
+rm -Rf "$BASE/pygame_sdl2/gen3-static"
+
