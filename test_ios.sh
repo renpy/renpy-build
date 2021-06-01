8a73104f95d1690d430cf2539f962764e5e2520f
Tom Rothamel <pytom@bishoujo.us>
2021-05-31 21:33:09 -0400

    Use rsync to test ios.
    Since smb on macOS is bad.

--- test_ios.sh
+++ test_ios.sh
@@ -4,3 +4,5 @@ set -e
 
 ./build.py --platform ios
 ./renpy/run.sh ./renpy/launcher ios_populate ${1:-./renpy/the_question} ./renios/prototype/
+
+rsync -a renios/prototype tom@mary21.local:/Users/tom/ios
