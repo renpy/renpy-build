e8876cabca0b3bde597f5a2f9d4279cbe7d13940
Liu Wenyuan <15816141883@163.com>
2021-08-22 16:44:51 +0800

    debig

--- rapt/buildlib/rapt/build.py
+++ rapt/buildlib/rapt/build.py
@@ -579,6 +579,8 @@ def build(iface, directory, install=False, bundle=False, launch=False, finished=
 
     if not bundle:
         apkdirs.append(plat.path("project/app/build/outputs/apk/release"))
+        for i in os.listdir(plat.path("project/app/build/outputs"):
+            print(i)
     else:
         apkdirs.append(plat.path("project/app/build/outputs/bundle/release"))
 
