194b1b79b592e57aaa91814a2635a36a91e34683
Tom Rothamel <pytom@bishoujo.us>
2022-03-11 23:04:59 -0500

    Remove colorama and non-Ren'Py iOS build script.

--- test_ios.sh
+++ test_ios.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./build.py --platform ios
+./build.py --platform ios --python 3
 ./renpy/run.sh ./renpy/launcher ios_populate ${1:-./renpy/the_question} ./renios/prototype/
 
 rsync -a renios/prototype tom@mary21.local:/Users/tom/ios
