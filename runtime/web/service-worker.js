4a1b1e11e7cb38d240cdd00a31d99c3114f00d27
Tom Rothamel <pytom@bishoujo.us>
2022-12-17 23:40:11 -0500

    web3 pwa: Redo how offline caching works.
    Rather than trying to load and sync the cache, we prefer fetch,
    which will use the cache if it can and the cache is up to date
    with the server.
    
    Only if the fetch fails does the service worker look in its
    cache, and try to fulfill the request that way.

--- runtime/web/service-worker.js
+++ runtime/web/service-worker.js
@@ -1,92 +1,46 @@
 var cacheName = 'renpy-web-game';
-var filesToCache = [
-    '/',
-    '/index.html',
-    '/game.zip',
-    '/renpy-pre.js',
-    '/renpy.js',
-    '/renpy.data',
-    '/renpy.wasm',
-    '/web-presplash.jpg'
-];
-
-install = async () => {
-    var cache = await caches.open(cacheName);
-    var catalog_response = await fetch('/pwa_catalog.json');
-    // copy the response so we can save it to cache
-    var catalog_response_copy = catalog_response.clone();
-    // Check if response is valid
-    if (catalog_response.status === 200) {
-        var pwa_catalog = await catalog_response.json();
-        var cached_pwa_response = await caches.match('/pwa_catalog.json');
-        // Get the response as json or set it as empty
-        var cached_pwa_catalog = null;
-        if (cached_pwa_response === undefined) {
-            cached_pwa_catalog = { "core": {} };
-        } else {
-            // Set response to cached_pwa_catalog
-            var cached_pwa_catalog = await cached_pwa_response.json();
-        }
-
-        for (var i = 0; i < filesToCache.length; i++) {
-            var url = filesToCache[i];
-            var cached_response = await caches.match(url);
-            var stripped_url = url.replace("/", "")
-            if (stripped_url == "") {
-                // If url is root, set it to index.html
-                stripped_url = "index.html"
-            }
-            if (cached_response) {
-                // Make sure the file is present in pwa_catalog.json
-                if (pwa_catalog["core"][stripped_url] !== undefined) {
-                    // Check if md5 hash has changed
-                    if (cached_pwa_catalog["core"][stripped_url] !== pwa_catalog["core"][stripped_url]) {
-                        console.log('Deleting ' + url + ' from current cache and retrieving it from the server');
-                        await cache.delete(url)
-                        await cache.add(url);
-                    } else {
-                        // Put existing response into the cache
-                        await cache.put(url, cached_response);
-                    }
-                }
-            } else {
-                // Cache not found, make a call to the server
-                await cache.add(url);
-            }
-        }
-        // Save pwa_catalog.json to cache
-        await cache.put('/pwa_catalog.json', catalog_response_copy);
-    }
-}
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
-    e.waitUntil(
-        install()
-    );
+    console.log('Service worker installed.');
     self.skipWaiting();
 });
 
+self.addEventListener('activate', function (e) {
+    return self.clients.claim();
+});
+
+/**
+ * Serves the cached version of the request if it exists, otherwise fetches the
+ * request from the network and caches it. Fetch is used in the default mode,
+ * which will use the cache for most network requests, freshening the cache
+ * as required.
+ */
+async function fetchAndCache(request) {
+    const cache = await caches.open(cacheName);
+
+    try {
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    } catch (e) {
+        try {
+            const response = await cache.match(request);
+            if (response) {
+                console.log('Served from cache: ' + request.url);
+                return response;
+            }
+        } catch (e) {
+            // pass
+        }
+
+        console.log('Not found in cache: ' + request.url);
+
+        throw e;
+    }
+}
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
-});
-
-/* Delete old caches */
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
+    e.respondWith(fetchAndCache(e.request));
 });
