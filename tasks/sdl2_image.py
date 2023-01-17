b3cd5fa56a7686ec9a2627a4f82bc1d928bf4a50
Tey <Tey@users.noreply.github.com>
2023-01-17 08:38:11 +0100

    Typo

--- tasks/sdl2_image.py
+++ tasks/sdl2_image.py
@@ -27,7 +27,7 @@ def build(c: Context):
 
     c.env("LIBAVIF_LIBS", "-lavif -laom")
 
-    if c.platform == "web" and c.platform != "2":
+    if c.platform == "web" and c.python != "2":
         c.env("SDL_CFLAGS", "-sUSE_SDL=2")
         c.env("SDL_LIBS", "-sUSE_SDL=2")
 
