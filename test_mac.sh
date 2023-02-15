01ca53da4fef015e378bb2aab8ebf258275830e5
Tom Rothamel <pytom@bishoujo.us>
2023-02-15 01:38:08 -0500

    mac: Remove argv emulation.
    At least on my M1 mac, Argv emulation doesn't seem to have
    worked since at least 7.3.5. Trying to run it seems to
    be causing crashes that prevent the Python 2 version from
    starting.  (See renpy/renpy#4346.)
    
    So, remove it entirely.

--- test_mac.sh
+++ test_mac.sh
@@ -21,8 +21,11 @@ sync lib/ lib
 sync rapt3/ rapt
 sync renios3/ renios
 sync renpy/ renpy
-sync renpy.app/ renpy.app
+sync renpy3.app/ renpy.app
+sync renpy3.app/ renpy3.app
+sync renpy2.app/ renpy2.app
 sync renpy.py renpy.py
 sync renpy.sh renpy.sh
+sync sdk-fonts/ sdk-fonts
 sync the_question/ the_question
 sync tutorial/ tutorial
