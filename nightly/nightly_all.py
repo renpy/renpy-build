b3d846047186c0bb54de5c13f9fcb25607e61047
Tom Rothamel <pytom@bishoujo.us>
2023-07-25 22:23:45 -0400

    nightly: Continue if one build fails to run.

--- nightly/nightly_all.py
+++ nightly/nightly_all.py
@@ -3,16 +3,23 @@
 import os
 import sys
 import subprocess
+import traceback
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
 def build_nightly():
 
     with open("/tmp/nightly-build-fix.txt", "w+") as f:
-        subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build-fix/', "fix", "--clean" ], stdout=f, stderr=f)
+        try:
+            subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build-fix/', "fix", "--clean" ], stdout=f, stderr=f)
+        except Exception as e:
+            traceback.print_exc(file=f)
 
     with open("/tmp/nightly-build-master.txt", "w+") as f:
-        subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build/', "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f)
+        try:
+            subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build/', "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f)
+        except Exception as e:
+            traceback.print_exc(file=f)
 
 if __name__ == "__main__":
     build_nightly()
