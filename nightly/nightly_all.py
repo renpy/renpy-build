7bbbdb53bde0147298bb890eefba7804f7bfcce0
Tom Rothamel <pytom@bishoujo.us>
2023-09-14 00:56:03 -0400

    Do not use nice when building nightlies.

--- nightly/nightly_all.py
+++ nightly/nightly_all.py
@@ -11,13 +11,13 @@ def build_nightly():
 
     with open("/tmp/nightly-build-fix.txt", "w+") as f:
         try:
-            subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build-fix/', "fix", "--clean" ], stdout=f, stderr=f)
+            subprocess.call([ "./nightly.sh", '/home/tom/ab/nightly-build-fix/', "fix", "--clean" ], stdout=f, stderr=f)
         except Exception as e:
             traceback.print_exc(file=f)
 
     with open("/tmp/nightly-build-master.txt", "w+") as f:
         try:
-            subprocess.call([ "nice", "./nightly.sh", '/home/tom/ab/nightly-build/', "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f)
+            subprocess.call([ "./nightly.sh", '/home/tom/ab/nightly-build/', "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f)
         except Exception as e:
             traceback.print_exc(file=f)
 
