cc653481fd0f012c5c97f6d91792c4b32b256fe7
Brad D <entertainment@brandongrows.me>
2024-03-25 21:04:48 -0500

    Fixes: Zlib + Bzip2 Compile Errors

--- tasks/zlib.py
+++ tasks/zlib.py
@@ -16,8 +16,20 @@ def unpack(c: Context):
 def build(c: Context):
     c.var("version", version)
     c.chdir("zlib-{{version}}")
+    if c.platform == "freebsd":
+        c.env("C_INCLUDE_PATH", "/usr/include:/usr/local/include")
+        c.env("CFLAGS", "{{ CFLAGS }} -L/usr/lib -L/usr/local/lib/gcc13")
+        # fix a weird linking bug where these system files were hardcoded
+        c.run("cp -rf /usr/lib/crt1.o .")
+        c.run("cp -rf /usr/lib/crti.o .")
+        c.run("cp -rf /usr/lib/crtbegin.o .")
+        c.run("cp -rf /usr/lib/crtend.o .")
+        c.run("cp -rf /usr/lib/crtn.o .")
+        # nuke the minigzip64 test; the bug was fixed in 1.2.13 but this was easier
+        c.run("cp /dev/null test/minigzip64.c")
     c.run("{{configure}} {{ configure_cross }} --static --prefix={{install}}")
-    c.run("{{ make }}")
+
+    c.run(""" {{ make }} """)
     c.run("{{ make }} install")
 
 
