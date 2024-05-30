a8bc046f75bf3b9403115de6f9b2dcbf90e58b98
Brainos <brainos233@gmail.com>
2024-05-30 15:28:43 +0800

    Upgrade ffmpeg to 4.4.4 to fix build this will fix `libavcodec/libdav1d.c:143:7: error: no member named 'n_tile_threads' in 'struct Dav1dSettings'` when build

--- tasks/ffmpeg.py
+++ tasks/ffmpeg.py
@@ -1,7 +1,7 @@
 from renpybuild.context import Context
 from renpybuild.task import task
 
-version = "4.3.1"
+version = "4.4.4"
 
 
 @task(platforms="all")
@@ -14,8 +14,10 @@ def unpack(c: Context):
     c.var("version", version)
     c.chdir("ffmpeg-{{version}}")
 
-    c.patch("ffmpeg-4.3.1-sse.diff")
+    # c.patch("ffmpeg-4.3.1-sse.diff")
     c.patch("ffmpeg-4.3.1-ff_seek_frame_binary.diff")
+    c.patch("ffmpeg-4.4.4-binutils-2.41.patch")
+    c.patch("ffmpeg-5.0-backport-ranlib-build-fix.patch")
 
 
 @task()
