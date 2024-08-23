644d4d79d034505e37e1ae0ac21c44252b93f8ab
Tom Rothamel <pytom@bishoujo.us>
2024-08-23 00:50:50 -0400

    Use the -fix checkout for nightly build.

--- nightly/nightly_all.py
+++ nightly/nightly_all.py
@@ -5,19 +5,22 @@ import sys
 import subprocess
 import traceback
 
+FIX_PATH = "/home/tom/ab/nightly-build-fix/"
+MASTER_PATH = "/home/tom/ab/nightly-build/"
+
 os.chdir(os.path.dirname(sys.argv[0]))
 
 def build_nightly():
 
     with open("/tmp/nightly-build-fix.txt", "w+") as f:
         try:
-            subprocess.call([ "./nightly.sh", '/home/tom/ab/nightly-build-fix/', "fix", "--upload", "--clean" ], stdout=f, stderr=f)
+            subprocess.call([ "./nightly.sh", FIX_PATH, "fix", "--upload", "--clean" ], stdout=f, stderr=f, cwd=FIX_PATH)
         except Exception as e:
             traceback.print_exc(file=f)
 
     with open("/tmp/nightly-build-master.txt", "w+") as f:
         try:
-            subprocess.call([ "./nightly.sh", '/home/tom/ab/nightly-build/', "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f)
+            subprocess.call([ "./nightly.sh", MASTER_PATH, "master", "--upload", "--prune", "--clean" ], stdout=f, stderr=f, cwd=MASTER_PATH)
         except Exception as e:
             traceback.print_exc(file=f)
 
