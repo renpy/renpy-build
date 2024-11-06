90211057e14069c9605de56ac9e06c2c050c7303
Tom Rothamel <pytom@bishoujo.us>
2024-11-05 23:33:26 -0500

    Revert "Fix stack size setting for older versions of emscripten."
    This reverts commit cf70c70019129ad440e8fd9184903c7b914167ef.

--- tasks/renpython.py
+++ tasks/renpython.py
@@ -606,7 +606,7 @@ def link_web(c: Context):
     -sASYNCIFY_ONLY="{{ asyncify_only }}"
     -sINITIAL_MEMORY=192MB
     -sALLOW_MEMORY_GROWTH=1
-    -sTOTAL_STACK=1048576
+    -sSTACK_SIZE=1024KB
 
     -sEXPORTED_FUNCTIONS=['_main']
 
