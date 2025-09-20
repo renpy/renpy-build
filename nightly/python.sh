95f75c507564d49da6d395c37eebb8e972795c17
Andy_kl <andy_kl@bk.ru>
2025-09-20 21:25:07 +0400

    Move dependencies into pyproject.toml.

--- nightly/python.sh
+++ nightly/python.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-if [ ! -e $VENV/bin ]; then
-    python3 -m venv $VENV
-fi
-
+uv sync
 
 pushd $BASE/renpy
 
@@ -14,9 +11,7 @@ uv sync
 . $VENV/bin/activate
 
 # Delete the generated files.
-rm -Rf renpy/module/gen-static
 rm -Rf renpy/module/gen3-static
-rm -Rf renpy/module/gen
 rm -Rf renpy/module/gen3
 
 ./run.sh launcher quit
