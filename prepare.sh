67fe526c292e6986bfdf4cfb3792c62576f977a8
Andy_kl <andy_kl@bk.ru>
2026-06-28 13:18:08 +0000

    Fixes for prepare script so it can run with no caches.

--- prepare.sh
+++ prepare.sh
@@ -12,15 +12,17 @@ export DEBIAN_FRONTEND=noninteractive
 
 mkdir -p $ROOT/tmp
 
+# Needed to install packages.
+sudo apt-get update
+
 # Needed to install LLVM.
-sudo apt install -y software-properties-common
+sudo apt-get install --no-install-recommends -y \
+    software-properties-common wget ca-certificates
 
 # Clang is needed to compile for many platforms.
 wget -O tmp/llvm.sh https://apt.llvm.org/llvm.sh
 chmod +x tmp/llvm.sh
 sudo tmp/llvm.sh $LLVM_MAJOR
-
-sudo apt-get update
 
 # Install all build and runtime dependencies in one command.
 #   git build-essential ccache ... : build tools
@@ -34,28 +36,28 @@ sudo apt-get update
 #   bc : needed by brotli
 #   ninja-build : needed for meson and cmake
 #   libassimp-dev libavcodec-dev ... : OS deps required by Ren'Py
-sudo apt-get install -y \
-        git build-essential ccache unzip autoconf autoconf-archive automake libtool-bin pkg-config \
-        llvm-$LLVM_MAJOR clang-$LLVM_MAJOR \
-        python3-dev python3-pip python3-venv \
-        curl \
-        python3-jinja2 \
-        debootstrap qemu-user-binfmt \
-        libgmp-dev libmpfr-dev libmpc-dev \
-        libssl-dev libbz2-dev liblzma-dev \
-        bc \
-        cmake ninja-build \
-        libassimp-dev \
-        libavcodec-dev \
-        libavformat-dev \
-        libswresample-dev \
-        libswscale-dev \
-        libharfbuzz-dev \
-        libfreetype6-dev \
-        libfribidi-dev \
-        libsdl3-dev \
-        libsdl3-image-dev \
-        libjpeg-dev
+sudo apt-get install --no-install-recommends -y \
+    git build-essential ccache unzip autoconf autoconf-archive automake libtool-bin pkg-config \
+    llvm-$LLVM_MAJOR clang-$LLVM_MAJOR \
+    python3-dev python3-pip python3-venv \
+    curl \
+    python3-jinja2 \
+    debootstrap qemu-user-binfmt \
+    libgmp-dev libmpfr-dev libmpc-dev \
+    libssl-dev libbz2-dev liblzma-dev \
+    bc \
+    cmake ninja-build \
+    libassimp-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libswresample-dev \
+    libswscale-dev \
+    libharfbuzz-dev \
+    libfreetype6-dev \
+    libfribidi-dev \
+    libsdl3-dev \
+    libsdl3-image-dev \
+    libjpeg-dev
 
 # Darwin clang_rt is needed to prevent undefined symbol: __isPlatformVersionAtLeast
 sudo tar xzf "$BASE/prebuilt/clang_rt.tar.gz" -C /usr/lib/clang/$LLVM_MAJOR/lib/
@@ -65,8 +67,6 @@ wget -qO- https://astral.sh/uv/install.sh | sh
 
 # Install the programs and virtualenvs (but not in a dev container - we use the
 # presence of nightly to check.)
-
-
 if [ -d "$ROOT/nightly" ]; then
     VENV="$ROOT/renpy/.venv"
 
