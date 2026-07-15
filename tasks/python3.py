6d773ba99a18dfc23b5a89e08feeef4d95353b38
Andy_kl <andy_kl@bk.ru>
2026-07-15 23:58:48 +0400

    Use the same flags to make libpython.a on web.

--- tasks/python3.py
+++ tasks/python3.py
@@ -240,16 +240,17 @@ def build_web(c: Context):
 
     c.chdir("Python-{{ version }}")
 
-    c.copy(
-        "{{ source }}/Python-{{ version }}-Setup.local",
-        "Modules/Setup.local",
-    )
+    patch_common(c)
+
+    c.run(""" autoreconf -vfi """)
 
     c.env("PYTHON_FOR_BUILD", "{{ host }}/web/bin/python3")
     c.env("CONFIG_SITE", "Tools/wasm/emscripten/config.site-wasm32-emscripten")
     c.env("CFLAGS", "{{ CFLAGS }} -DPY_CALL_TRAMPOLINE")
     c.env("CXXFLAGS", " {{ CXXFLAGS }} -DPY_CALL_TRAMPOLINE")
     c.env("PKG_CONFIG", "pkg-config")
+    c.env("MODULE_BUILDTYPE", "static")
+    c.env("LIBHACL_LDEPS_LIBTYPE", "STATIC")
 
     c.run("""
         {{configure}} {{ cross_config }}
@@ -266,15 +267,7 @@ def build_web(c: Context):
 
     # c.run("""python3 {{ root }}/tools/opfunc/opfunc_transform.py Python/ceval.c""")
 
-    # c.run("""{{ make }}""")
-
-    c.run("""{{ make }} install""")
-    c.copy("{{ host }}/bin/python3", "{{ install }}/bin/hostpython3")
-
-    for i in ["_sysconfigdata__linux_x86_64-linux-gnu.py"]:
-        c.var("i", i)
-
-        c.copy("{{ host }}/lib/{{pythonver}}/{{ i }}", "{{ install }}/lib/{{pythonver}}/{{ i }}")
+    common_post(c)
 
 
 def get_uv_lock_versions(lock_path: str | Path) -> dict[str, str]:
