6c4fbfdec45d557dfd1f0abf737c0e89e7f21c08
Andy_kl <andy_kl@bk.ru>
2026-07-19 04:31:38 +0400

    Make sure angle libs are copied.

--- tasks/angle.py
+++ tasks/angle.py
@@ -2,9 +2,8 @@ from renpybuild.context import Context
 from renpybuild.task import task
 
 
-@task(kind="arch", platforms="windows")
+@task(kind="arch", platforms="windows", always=True)
 def install(c: Context):
     c.run("install -d {{ dlpa }}")
     c.run("install {{ prebuilt }}/{{ c.arch}}/libGLESv2.dll {{ dlpa }}")
     c.run("install {{ prebuilt }}/{{ c.arch }}/libEGL.dll {{ dlpa }}")
-    c.run("install {{ prebuilt }}/{{ c.arch }}/d3dcompiler_47.dll {{ dlpa }}")
